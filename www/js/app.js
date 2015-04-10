0d61220f26345fdcd8afbe2c5641eed2dedf7810
cefalo-rakib <rakib.shahriar@cefalo.com>
2015-04-11 05:10:16 +0600

    airbop works

--- www/js/app.js
+++ www/js/app.js
@@ -1,71 +1,144 @@
-// Ionic Starter App
+angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'notifyme.services'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+    .run(function ($rootScope, $state, $ionicPlatform, $window, $cordovaPush, $http, Commons) {
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+        var androidConfig = {
+            "senderID": "341139902696"
+        };
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+        var AIRBOP_APP_KEY = Commons.getAirbopAppKey();
+        var AIRBOP_APP_SECRET = Commons.getAirbopAppSecret();
 
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
 
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
+        document.addEventListener("deviceready", function () {
 
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
+            $cordovaPush.register(androidConfig).then(function (result) {
+                // Success
+                console.log("Registered with : " + result);
+            }, function (err) {
+                // Error
+                Commons.setRegistrationValue('');
+                console.log("Registration failed with : " + err);
+            })
+
+            $rootScope.$on('$cordovaPush:notificationReceived', function (event, notification) {
+                switch (notification.event) {
+                    case 'registered':
+                        if (notification.regid.length > 0) {
+                            // Set registration id to local storage
+                            Commons.setRegistrationValue(notification.regId);
+
+                            // Register with Airdrop
+                            var request_uri = Commons.getAirbopRegUrl();
+                            var request_body = JSON.stringify({
+                                'reg': notification.regid
+                            });
+                            var timestamp = Math.round(new Date() / 1000);
+                            var signature = CryptoJS.SHA256("POST" + request_uri + AIRBOP_APP_KEY
+                                                        + timestamp + request_body + AIRBOP_APP_SECRET);
+
+                            $http({
+                                'method': 'POST',
+                                'url': request_uri,
+                                'dataType': "json",
+                                'headers': {
+                                    'Content-Type': 'application/json',
+                                    'x-app-key': AIRBOP_APP_KEY,
+                                    'x-timestamp': timestamp,
+                                    'x-signature': signature
+                                },
+                                'data': request_body
+                            }).success(function (data, status, headers, config) {
+                                console.log("Connection with Airbop successful with status :" + status);
+                            }).error(function (data, status, headers, config) {
+                                console.log("Connection with Airbop failed with status :" + status);
+                            });
+                        }
+                        break;
+
+                    case 'message':
+                        // this is the actual push notification. its format depends on the data model from the push server
+                        alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
+                        break;
+
+                    case 'error':
+                        alert('GCM error = ' + notification.msg);
+                        break;
+
+                    default:
+                        alert('An unknown GCM event has occurred');
+                        break;
+                }
+            });
+
+
+            // WARNING: dangerous to unregister (results in loss of tokenID)
+            $cordovaPush.unregister(options).then(function (result) {
+                // Success!
+            }, function (err) {
+                // Error
+            })
+
+        }, false);
     })
 
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/menu.html",
+                controller: 'AppCtrl'
+            })
+
+            .state('app.search', {
+                url: "/search",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/search.html"
+                    }
+                }
+            })
+
+            .state('app.browse', {
+                url: "/browse",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/browse.html"
+                    }
+                }
+            })
+            .state('app.playlists', {
+                url: "/playlists",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/playlists.html",
+                        controller: 'PlaylistsCtrl'
+                    }
+                }
+            })
+
+            .state('app.single', {
+                url: "/playlists/:playlistId",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/playlist.html",
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/playlists');
+    });
---
+++ www/js/services.js
@@ -0,0 +1,95 @@
+angular.module('notifyme.services', [])
+
+    .factory('Commons', function ($window, Storage) {
+        var registrationIdKey = 'registrationId';
+        return {
+            isRegistered: function() {
+                if (Storage.getLocalStorageMapAsString(registrationIdKey)) {
+                    return true;
+                }
+                ;
+                return false;
+            },
+
+            getRegistrationKey: function() {
+                return registrationIdKey;
+            },
+            setRegistrationValue: function(registrationId) {
+                Storage.setLocalStorageMapAsString(registrationIdKey, registrationId);
+            },
+
+            getAirbopRegUrl : function () {
+                return 'http://www.airbop.com/api/v1/register';
+            },
+            getAirbopAppKey : function() {
+                return '0486ff8d-ab01-477f-8467-63132d0cd1c8';
+            },
+            getAirbopAppSecret : function() {
+                return '8ea8e4441e63835047cf85ab1da3611c2e6e0e0802117361bfefe116b11daadf';
+            }
+        };
+    })
+
+    .factory('Storage', function($window) {
+        return {
+            getLocalStorage: function() {
+                return $window.localStorage;
+            },
+
+            getLocalStorageMapValue: function(keyName) {
+                return JSON.parse($window.localStorage[keyName] || '[]');
+            },
+            setLocalStorageMapValue: function(keyName, keyValue) {
+                $window.localStorage['feeds'] = JSON.stringify(keyValue);
+            },
+
+            getLocalStorageMapAsString: function(keyName) {
+                return $window.localStorage[keyName];
+            },
+            setLocalStorageMapAsString: function(keyName, keyValue) {
+                $window.localStorage[keyName] = keyValue;
+            }
+        };
+    })
+
+    .factory('Feeds', function ($window) {
+        return {
+            all: function () {
+                var feedsLoaded = JSON.parse($window.localStorage['feeds'] || '[]');
+                return feedsLoaded;
+            },
+            getFeed: function (notificationId) {
+                var feeds = JSON.parse($window.localStorage['feeds'] || '[]');
+                var index = feeds.map(function (x) {
+                    return x.notificationId;
+                }).indexOf(notificationId);
+                return feeds[index];
+            },
+            addFeed: function (feed) {
+                var feeds = JSON.parse($window.localStorage['feeds'] || '[]');
+                var found = false;
+                for (var i = 0; i < feeds.length; i++) {
+                    if (feeds[i].notificationId == feed.notificationId) {
+                        found = true;
+                    }
+                    ;
+                }
+                if (!found) {
+                    feeds.unshift(feed);
+                    $window.localStorage['feeds'] = JSON.stringify(feeds);
+                }
+                ;
+            },
+            removeFeed: function (feed) {
+                var feeds = JSON.parse($window.localStorage['feeds'] || '[]');
+                var index = feeds.map(function (x) {
+                    return x.notificationId;
+                }).indexOf(feed.notificationId);
+                feeds.splice(index, 1);
+                $window.localStorage['feeds'] = JSON.stringify(feeds);
+            },
+            clearFeeds: function () {
+                $window.localStorage['feeds'] = '[]';
+            }
+        };
+    });
